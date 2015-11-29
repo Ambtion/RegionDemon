ca987df446e83fb78b7e4492e0b218c597cf03d8
Qu Ke <kequ@sohu-inc.com>
2015-11-29 22:23:21 +0800

    Initial Commit

---
+++ FenceDemon/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  FenceDemon
+//
+//  Created by kequ on 15/11/29.
+//  Copyright © 2015年 ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
